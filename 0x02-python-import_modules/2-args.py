77ff48d258a3e625b720c5d42ef5fb867f6f43ac
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-06-08 16:09:08 +0000

    Add Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    cnt = len(sys.argv) - 1
+
+    if cnt == 0:
+        print("0 arguments.")
+    elif cnt == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(cnt))
+
+    for i, arg in enumerate(sys.argv):
+        if i:
+            print("{}: {}".format(i, arg))
